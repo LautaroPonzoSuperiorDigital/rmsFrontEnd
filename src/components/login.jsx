440bcdee1376c87a5cd023e35835e028d486992b
LautaroP <lautaro@superiordigital.design>
2023-07-18 20:21:00 -0300

    fix

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
   const handleLogin = async () => {
     try {
       const response = await axios.post(
-        `https://https://api.certifymyrent.com/auth/local/signin`,
+        `https://api.certifymyrent.com/auth/local/signin`,
         {
           email,
           password,
