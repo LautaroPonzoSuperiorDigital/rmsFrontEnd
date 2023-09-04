e33b0aa5533d7ea8d03759a0c4d65a4f2373202a
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-09-04 11:04:43 -0300

    draft

--- src/components/listing-inspection-history/index.jsx
+++ src/components/listing-inspection-history/index.jsx
@@ -1,8 +1,15 @@
-import { useCallback, useEffect, useState } from "react";
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
 import PropTypes from "prop-types";
 import { api } from "../../services/api";
+import { isAxiosError } from "axios";
 import { ListingInspectionHistoryCard } from "./styles";
-import InspectionFormModal from "../inspection-form";
 import Edit from "../../assets/img/Edit.svg";
 import EditHover from "../../assets/img/EditHover.svg";
 import Delete from "../../assets/img/delete.svg";
@@ -11,35 +18,40 @@ import {
   DeleteInspectionButton,
   EditInspectionButton,
 } from "../buttonInspections";
+import { Modal } from "../modal";
+import { InspectionForm } from "../inspection-form/index";
+import { DeleteInspection } from "../delete-inspection/index";
+// import { Edit, Trash } from '../icons'
 
-const ListingInspectionHistoryWithRef = ({ listingId }) => {
+const ListingInspectionHistoryWithRef = ({ listingId }, ref) => {
   const [inspections, setInspections] = useState([]);
-  const [isInspectionModalOpen, setInspectionModalOpen] = useState(false);
+  const [editingInspection, setEditingInspection] = useState(null);
+  const [inspectionToDelete, setInspectionToDelete] = useState(null);
 
-  const handleUpdateInspection = (listingId, inspectionId) => {
-    console.log({ clicked: true });
-    openModal();
+  const deleteInspectionModalRef = useRef(null);
+  const inspectionFormModalRef = useRef(null);
+  const inspectionFormRef = useRef(null);
 
-    return (
-      <InspectionFormModal
-        key={`listing$${listingId}-inspection-${inspectionId}`}
-        isOpen={isInspectionModalOpen}
-        onClose={closeModal}
-        listingId={listingId}
-        inspectionId={inspectionId}
-      />
-    );
+  useImperativeHandle(ref, () => ({
+    openForm: handleOpenInspectionFormModal,
+  }));
+
+  const handleOpenInspectionFormModal = () => {
+    inspectionFormModalRef.current?.open();
   };
 
-  const handleDeleteInspection = async (listingId, inspectionId) => {
-    if (!(listingId && inspectionId)) return;
+  const handleCloseRemoveInspectionModal = () => {
+    deleteInspectionModalRef.current.close();
+    setInspectionToDelete(null);
+  };
 
+  const handleDeleteInspection = async () => {
     try {
-      await api.delete(`listing/${listingId}/inspection/${inspectionId}`);
+      await api.delete(`listing/${listingId}/inspection/${inspectionToDelete}`);
 
-      onInspectionDeleted(inspectionId);
+      onInspectionDeleted(inspectionToDelete);
     } catch (err) {
-      alert("Error deleting expense");
+      alert("Error deleting inspection");
     }
   };
 
@@ -49,12 +61,100 @@ const ListingInspectionHistoryWithRef = ({ listingId }) => {
     );
   }, []);
 
-  const openModal = () => {
-    setInspectionModalOpen(true);
+  const onInspectionAdded = useCallback(
+    (newInspection) => {
+      const _inspections = [...inspections, newInspection];
+      _inspections.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+      setInspections(_inspections);
+    },
+    [inspections]
+  );
+
+  const onInspectionChanged = useCallback(
+    (updatedInspection) => {
+      const _inspections = [...inspections];
+
+      const inspectionIndex = _inspections.findIndex(
+        (inspection) => inspection.id === updatedInspection.id
+      );
+
+      if (inspectionIndex === -1) return;
+
+      _inspections.splice(inspectionIndex, 1, updatedInspection);
+      _inspections.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+      setInspections(_inspections);
+    },
+    [inspections]
+  );
+
+  const handleCancelAddInspection = () => {
+    inspectionFormModalRef.current?.close();
+    setEditingInspection(false);
   };
 
-  const closeModal = () => {
-    setInspectionModalOpen(false);
+  useImperativeHandle(ref, () => ({
+    openForm: handleOpenInspectionFormModal,
+  }));
+
+  const handleSaveInspection = async () => {
+    const inspectionData = inspectionFormRef.current?.getData();
+
+    const needToFill = Object.keys(inspectionData).some(
+      (key) => !inspectionData[key]
+    );
+
+    if (needToFill) {
+      alert("Please fill all the fields.");
+      return;
+    }
+
+    try {
+      if (editingInspection) {
+        const { data: updatedInspection } = await api.patch(
+          `/listing/${listingId}/inspection/1${editingInspection.id}`,
+          {
+            name: inspectionData.name,
+            date: inspectionData.date,
+            type: "Regular",
+          }
+        );
+
+        onInspectionChanged(updatedInspection);
+      } else {
+        const { data: newInspection } = await api.post(
+          `/listing/${listingId}/inspection`,
+          {
+            name: inspectionData.name,
+            date: inspectionData.date,
+            type: inspectionData.type,
+          }
+        );
+
+        onInspectionAdded(newInspection);
+      }
+
+      inspectionFormModalRef.current.close();
+    } catch (err) {
+      if (isAxiosError(err)) {
+        if (err.response.data.response?.response?.message) {
+          const errorMessages = err.response.data.response.response.message;
+          const alertMessage = errorMessages
+            .map((message) =>
+              Object.keys(message.constraints).map(
+                (key) => message.constraints[key]
+              )
+            )
+            .join(", ");
+
+          alert(alertMessage);
+          return;
+        }
+      }
+
+      alert("Error saving inspection.");
+    }
   };
 
   useEffect(() => {
@@ -63,32 +163,30 @@ const ListingInspectionHistoryWithRef = ({ listingId }) => {
         const { data } = await api.get(`/listing/${listingId}/inspection`);
         setInspections(data);
       } catch (err) {
-        alert("Error loading listing expenses.");
+        alert("Error loading listing inspections.");
       }
     }
 
     loadInspections();
-  }, [listingId, inspections]);
+  }, [listingId]);
 
   return (
     <ListingInspectionHistoryCard>
       <div className="inspection-card-container">
         {inspections.map((inspection) => (
-          <div
-            className="inspection-card"
-            key={inspection.id}
-            onClick={() => handleUpdateInspection(listingId, inspection.id)}
-          >
+          <div className="inspection-card" key={inspection.id}>
             <div className="inspectionContent ms-3 mt-2 me-3">
               <div className="actionButtons">
                 <EditInspectionButton
+                  key={`edit-listing${listingId}-inspection${inspection.id}`}
                   defaultImage={<img src={Edit} alt="Edit" />}
                   hoverImage={<img src={EditHover} alt="EditHover" />}
-                  onClick={() =>
-                    handleUpdateInspection(listingId, inspection.id)
-                  }
+                  // onClick={() =>
+                  //   handleUpdateInspection(listingId, inspection.id)
+                  // }
                 />
                 <DeleteInspectionButton
+                  key={`delete-listing${listingId}-inspection${inspection.id}`}
                   className="delete"
                   defaultImage={<img src={Delete} alt="Delete" />}
                   hoverImage={
@@ -110,6 +208,59 @@ const ListingInspectionHistoryWithRef = ({ listingId }) => {
             </div>
           </div>
         ))}
+        <Modal.Root
+          ref={inspectionFormModalRef}
+          onModalClosed={() => setEditingInspection(null)}
+        >
+          <Modal.Body width="100%">
+            <Modal.Header showCloseIcon />
+            <Modal.Content>
+              <InspectionForm
+                ref={inspectionFormRef}
+                inspection={editingInspection}
+              />
+            </Modal.Content>
+            <Modal.Footer>
+              <Modal.Action
+                outline
+                text="Cancel"
+                action={handleCancelAddInspection}
+              />
+              <Modal.Action text="Save" action={handleSaveInspection} />
+            </Modal.Footer>
+          </Modal.Body>
+        </Modal.Root>
+        <Modal.Root
+          ref={deleteInspectionModalRef}
+          onModalClosed={() => setInspectionToDelete(null)}
+        >
+          <Modal.Body>
+            <Modal.Header showCloseIcon />
+            <Modal.Content>
+              <DeleteInspection inspection={inspectionToDelete} />
+            </Modal.Content>
+            <Modal.Footer>
+              <Modal.Action
+                text="Cancel"
+                action={handleCloseRemoveInspectionModal}
+                outline
+              />
+              <Modal.Action
+                text="Delete"
+                action={handleDeleteInspection}
+                danger
+              />
+            </Modal.Footer>
+          </Modal.Body>
+        </Modal.Root>
+
+        {/* <InspectionFormModal
+          key={`listing$${listingId}-inspection-${selectedInspection}`}
+          isOpen={isOpenModal}
+          onClose={closeModal}
+          listingId={listingId}
+          inspectionId={selectedInspection}
+        /> */}
       </div>
     </ListingInspectionHistoryCard>
   );
@@ -119,4 +270,6 @@ ListingInspectionHistoryWithRef.propTypes = {
   listingId: PropTypes.number.isRequired,
 };
 
-export const ListingInspectionHistory = ListingInspectionHistoryWithRef;
+export const ListingInspectionHistory = forwardRef(
+  ListingInspectionHistoryWithRef
+);
