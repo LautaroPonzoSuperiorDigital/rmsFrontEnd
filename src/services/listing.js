4a88dc710937a89c76529b4b0a4ac2529c7301cc
charlieB <charlie@superiordigital.design>
2023-09-04 19:51:32 -0300

    fix:images nulls

--- src/services/listing.js
+++ src/services/listing.js
@@ -1,7 +1,8 @@
 export const createListingImage = (listing) => {
+  console.log(listing);
   let image = null;
 
-  if (listing.Sections[0]?.Album.Images[0]) {
+  if (listing.Sections[0]?.Album?.Images[0]) {
     const key = listing.Sections[0]?.Album.Images[0].key;
 
     image = `https://rms-staging.s3.us-west-1.amazonaws.com/${key}`.replace(
