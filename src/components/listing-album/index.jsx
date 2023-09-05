5e49dc1958b7a5e945c1dd19c14442cd47d2dce6
charlieB <charlie@superiordigital.design>
2023-09-05 09:43:13 -0300

    fix images

--- src/components/listing-album/index.jsx
+++ src/components/listing-album/index.jsx
@@ -1,12 +1,12 @@
-import PropTypes from "prop-types"
-import { useCallback, useEffect, useRef, useState } from "react"
-import { FiCheck, FiX } from "react-icons/fi"
-import { Tabs } from "react-tabs"
+import PropTypes from "prop-types";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { FiCheck, FiX } from "react-icons/fi";
+import { Tabs } from "react-tabs";
 
-import { toBase64 } from "../../services/image"
-import { useModal } from "../modal/context"
+import { toBase64 } from "../../services/image";
+import { useModal } from "../modal/context";
 
-import { Edit, Trash } from "../icons"
+import { Edit, Trash } from "../icons";
 
 import {
   AddPhotosButton,
@@ -26,7 +26,7 @@ import {
   SectionTabs,
   SectionsActions,
   UploadPhotosInput,
-} from "./styles"
+} from "./styles";
 
 export function ListingAlbum({
   handleGoBack,
@@ -37,94 +37,104 @@ export function ListingAlbum({
   onImagesUploaded,
   onImageRemoved,
 }) {
-  const [activeSectionIndex, setActiveSectionIndex] = useState(0)
-  const [edit, setEdit] = useState(false)
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [edit, setEdit] = useState(false);
 
-  const { showFooter, hideFooter, setHeight } = useModal()
+  const { showFooter, hideFooter, setHeight } = useModal();
 
-  const uploadPhotosRef = useRef(null)
-  const editInputRef = useRef(null)
+  const uploadPhotosRef = useRef(null);
+  const editInputRef = useRef(null);
 
   const toggleEdit = () => {
-    if(!editable) {
-      return
+    if (!editable) {
+      return;
     }
 
-    setEdit(old => !old)
-  }
+    setEdit((old) => !old);
+  };
 
   const handleSaveName = () => {
     if (!editable || !editInputRef.current || !onSectionNameChange) {
-      return
+      return;
     }
 
-    onSectionNameChange(activeSectionIndex, editInputRef.current.value)
-    setEdit(false)
-  }
+    onSectionNameChange(activeSectionIndex, editInputRef.current.value);
+    setEdit(false);
+  };
 
   const handleOpenUpload = () => {
     if (!editable) {
-      return
+      return;
     }
 
-    uploadPhotosRef.current.click()
-  }
+    uploadPhotosRef.current.click();
+  };
 
-  const onUploaded = useCallback((event) => {
-    if (!editable && !onImagesUploaded) {
-      return
-    }
-
-    const filesArray = Array.from(event.target.files)
-    const images = filesArray.map(file => {
-      const url = URL.createObjectURL(file)
-
-      const image = {
-        url,
-        base64: ''
+  const onUploaded = useCallback(
+    (event) => {
+      if (!editable && !onImagesUploaded) {
+        return;
       }
 
-      toBase64(file).then(base64 => {
-        image.base64 = base64
-      })
+      const filesArray = Array.from(event.target.files);
+      const images = filesArray.map((file) => {
+        const url = URL.createObjectURL(file);
 
-      return image
-    })
+        const image = {
+          url,
+          base64: "",
+        };
 
-    onImagesUploaded(activeSectionIndex, images)
-  }, [editable, activeSectionIndex, onImagesUploaded])
+        toBase64(file).then((base64) => {
+          image.base64 = base64;
+        });
 
-  const handleRemoveImage = useCallback((imageIndex) => {
-    if (!editable || !onImageRemoved) {
-      return
-    }
+        return image;
+      });
 
-    onImageRemoved(activeSectionIndex, imageIndex)
-  }, [editable, activeSectionIndex, onImageRemoved])
+      onImagesUploaded(activeSectionIndex, images);
+    },
+    [editable, activeSectionIndex, onImagesUploaded]
+  );
+
+  const handleRemoveImage = useCallback(
+    (imageIndex) => {
+      if (!editable || !onImageRemoved) {
+        return;
+      }
+
+      onImageRemoved(activeSectionIndex, imageIndex);
+    },
+    [editable, activeSectionIndex, onImageRemoved]
+  );
 
   useEffect(() => {
-    hideFooter()
+    hideFooter();
 
-    return showFooter
-  }, [hideFooter, showFooter])
+    return showFooter;
+  }, [hideFooter, showFooter]);
 
   useEffect(() => {
-    setHeight('90vh')
+    setHeight("90vh");
 
-    return () => setHeight(undefined)
-  }, [setHeight])
+    return () => setHeight(undefined);
+  }, [setHeight]);
 
   return (
     <ListingAlbumContainer>
       <GoBackButton type="button" onClick={handleGoBack}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="8.974" height="15.592" viewBox="0 0 8.974 15.592">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="8.974"
+          height="15.592"
+          viewBox="0 0 8.974 15.592"
+        >
           <path
             d="M8.349,13.654a1.046,1.046,0,0,0,.36.8l6.838,6.7a1,1,0,0,0,.729.3A1.037,1.037,0,0,0,17.322,20.4a1.074,1.074,0,0,0-.316-.747l-6.161-6,6.161-6a1.09,1.09,0,0,0,.316-.747,1.037,1.037,0,0,0-1.046-1.046,1,1,0,0,0-.729.3L8.709,12.846A1.078,1.078,0,0,0,8.349,13.654Z"
             transform="translate(-8.349 -5.858)"
             fill="#197572"
           />
         </svg>
-
         Go Back
       </GoBackButton>
 
@@ -167,7 +177,7 @@ export function ListingAlbum({
               )}
             </SectionTab>
           ))}
-          
+
           {editable && (
             <SectionsActions>
               <NewSectionButton type="button" onClick={handleAddSection}>
@@ -183,15 +193,19 @@ export function ListingAlbum({
 
         {listingSections.map((section, index) => (
           <SectionTabContent key={index}>
-            {section?.Album.Images.length ? (
+            {section?.Album?.Images?.length ? (
               <SectionTabImageGrid>
                 {section.Album.Images.map((image, index) => {
-                  let url
+                  let url;
 
                   if (image.key) {
-                    url = `https://rms-staging.s3.us-west-1.amazonaws.com/${image.key}`.replace(/\\/g, "%5C")
+                    url =
+                      `https://rms-staging.s3.us-west-1.amazonaws.com/${image.key}`.replace(
+                        /\\/g,
+                        "%5C"
+                      );
                   } else {
-                    url = image.url
+                    url = image.url;
                   }
 
                   return (
@@ -199,12 +213,15 @@ export function ListingAlbum({
                       <SectionImage src={url} />
 
                       {editable && (
-                        <RemoveImageButton type="button" onClick={() => handleRemoveImage(index)}>
+                        <RemoveImageButton
+                          type="button"
+                          onClick={() => handleRemoveImage(index)}
+                        >
                           <Trash />
                         </RemoveImageButton>
                       )}
                     </SectionImageContainer>
-                  )
+                  );
                 })}
               </SectionTabImageGrid>
             ) : (
@@ -213,7 +230,7 @@ export function ListingAlbum({
           </SectionTabContent>
         ))}
       </Tabs>
-      
+
       {editable && (
         <UploadPhotosInput
           ref={uploadPhotosRef}
@@ -223,7 +240,7 @@ export function ListingAlbum({
         />
       )}
     </ListingAlbumContainer>
-  )
+  );
 }
 
 ListingAlbum.propTypes = {
@@ -234,4 +251,4 @@ ListingAlbum.propTypes = {
   onSectionNameChange: PropTypes.func,
   onImagesUploaded: PropTypes.func,
   onImageRemoved: PropTypes.func,
-}
+};
