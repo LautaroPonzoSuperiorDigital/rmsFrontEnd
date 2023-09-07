cdfa6068267323d00722a246e1307d78a966cee7
charlieB <charlie@superiordigital.design>
2023-09-07 14:16:03 -0300

    add carousel

--- src/routes/public.routes.jsx
+++ src/routes/public.routes.jsx
@@ -1,13 +1,14 @@
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import Login from "../components/login";
 import PublicListings from "../components/public/publicListings";
+import ModalPublicListings from "../components/public/modalPublicListings";
 
 const publicRoutes = [
-  { path: '/login', element: <Login /> },
-  { path: '/', element: <PublicListings /> },
-  { path: '*', element: <Navigate to="/login" replace /> }
-]
+  { path: "/login", element: <Login /> },
+  { path: "/", element: <PublicListings /> },
+  { path: "*", element: <Navigate to="/login" replace /> },
+];
 
-const publicRouter = createBrowserRouter(publicRoutes)
+const publicRouter = createBrowserRouter(publicRoutes);
 
-export const createPublicRouter = () => publicRouter
+export const createPublicRouter = () => publicRouter;
