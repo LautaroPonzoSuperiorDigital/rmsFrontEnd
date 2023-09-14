37ce500f79a135f3e2090434cb48bdac2d2d7330
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-09-14 11:29:03 -0300

    Update styles.js

--- src/components/listing-inspection-section-category-images/styles.js
+++ src/components/listing-inspection-section-category-images/styles.js
@@ -1,38 +1,32 @@
-import { styled } from "styled-components"
+import { styled } from "styled-components";
 
 export const ListingInspectionSectionCategoryImagesContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
   position: relative;
-
   user-select: none;
 
-  >svg {
+  > svg {
     width: 3rem;
     height: 3rem;
 
     cursor: pointer;
   }
 
-  >img {
+  > img {
     width: 100%;
     height: 70vh;
-
     object-fit: contain;
   }
-`
+`;
 
 export const InspectionImagesDetailsBox = styled.div`
   position: absolute;
   bottom: 1rem;
   left: 50%;
   transform: translateX(-50%);
-
   background: rgba(253, 253, 253, 0.85);
-
   color: #197572;
-
   padding: 0.375rem 0.5rem;
-`
+`;
