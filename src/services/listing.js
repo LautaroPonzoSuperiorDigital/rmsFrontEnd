c601169917c6774d223743ee7a5814c27e3c8f1e
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-09-18 15:32:28 -0300

    refactor: public listings image display

--- src/services/listing.js
+++ src/services/listing.js
@@ -1,16 +1,24 @@
 import DefaultImage from "./../assets/img/defaultImage.png";
+import { api } from "./api";
 
-export const createListingImage = (listing) => {
+export const createListingImage = (listing, callback) => {
   let image = DefaultImage;
 
-  if (listing.Sections[0]?.Album?.Images[0]) {
-    const key = listing.Sections[0]?.Album.Images[0].key;
-
-    image = `https://rms-staging.s3.us-west-1.amazonaws.com/${key}`.replace(
-      /\\/g,
-      "%5C"
-    );
-  }
-
-  return image;
+  api
+    .get(`/listing/${listing.id}/album`)
+    .then((response) => {
+      const data = response.data;
+      if (data?.Sections[0]?.Images[0]) {
+        const key = data?.Sections[0]?.Images[0].key;
+        image = `https://rms-staging.s3.us-west-1.amazonaws.com/${key}`.replace(
+          /\\/g,
+          "%5C"
+        );
+      }
+      callback(image);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      callback(image); // Return the default image in case of an error
+    });
 };
