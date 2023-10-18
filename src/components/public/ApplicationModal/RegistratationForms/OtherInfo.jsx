bbc625fa1e9722a49699f0dc575498ddb555fde5
charlieB <charlie@superiordigital.design>
2023-10-18 08:56:36 -0300

    finish other info form

--- src/components/public/ApplicationModal/RegistratationForms/OtherInfo.jsx
+++ src/components/public/ApplicationModal/RegistratationForms/OtherInfo.jsx
@@ -1,61 +1,118 @@
 import React, { useState } from "react"
 
 const OtherInfo = () => {
-  const [toggleOn, setToggleOn] = useState(false)
-  const [toggleOn1, setToggleOn1] = useState(false)
-  const [toggleOn2, setToggleOn2] = useState(false)
-  const [toggleOn3, setToggleOn3] = useState(false)
-  const [toggleOn4, setToggleOn4] = useState(false)
-  const [toggleOn5, setToggleOn5] = useState(false)
-  const handleClickToggle1 = () => {
-    setToggleOn1(!toggleOn1)
+  const [formData, setFormData] = useState({
+    evicted: { checked: false, details: "" },
+    convicted: { checked: false, details: "" },
+    bankruptcy: { checked: false, details: "" },
+    smoke: { checked: false },
+    pets: { checked: false, petDetails: "" },
+    learnAboutUs: ""
+  })
+
+  const handleCheckboxChange = (name, isChecked) => {
+    setFormData({
+      ...formData,
+      [name]: {
+        ...formData[name],
+        checked: isChecked
+      }
+    })
   }
 
-  const handleClickToggle2 = () => {
-    setToggleOn2(!toggleOn2)
+  const handleDetailsChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: {
+        ...formData[name],
+        details: value
+      }
+    })
   }
 
-  const handleClickToggle3 = () => {
-    setToggleOn3(!toggleOn3)
+  const handlePetDetailsChange = (value) => {
+    setFormData({
+      ...formData,
+      pets: {
+        ...formData.pets,
+        petDetails: value
+      }
+    })
   }
-  const handleClickToggle4 = () => {
-    setToggleOn4(!toggleOn4)
+
+  const handleLearnAboutUsChange = (value) => {
+    setFormData({
+      ...formData,
+      learnAboutUs: value
+    })
   }
-  const handleClickToggle5 = () => {
-    setToggleOn5(!toggleOn5)
+
+  const handleSubmit = () => {
+    // You can access the form data in the `formData` state object
+    console.log(formData)
+    // Perform any further actions, like sending the data to a server
   }
 
   return (
-    <div className=" d-flex flex-column align-items-center ">
-      <h2 className="mb-5 ">Other Information</h2>
+    <div className=" d-flex flex-column align-items-center w-100 ">
+      <h2 className="mb-5">Other Information</h2>
       <div className="orderInfo d-flex align-items-center">
         <div className="leftBox d-flex align-items-start justify-content-center">
           <div className="leftSideInfo d-flex">
             <p className="infoP">Have You Ever Been Evicted?</p>
-            <toogle />
+            <div className="form-check form-switch">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="flexSwitchCheckDefault1"
+                checked={formData.evicted.checked}
+                onChange={(e) =>
+                  handleCheckboxChange("evicted", e.target.checked)
+                }
+              />
+            </div>
           </div>
           <div className="inputInfoH">
-            {toggleOn1 && (
+            {formData.evicted.checked && (
               <input
                 className="inputReset4"
                 type="text"
                 placeholder="WHEN & WHY"
+                value={formData.evicted.details}
+                onChange={(e) => handleDetailsChange("evicted", e.target.value)}
               />
             )}
           </div>
           <div className="leftSideInfo d-flex">
             <p className="infoP">Have You Ever Been Convicted Of A Felony?</p>
-            <toogle />
+            <div className="form-check form-switch">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="flexSwitchCheckDefault2"
+                checked={formData.convicted.checked}
+                onChange={(e) =>
+                  handleCheckboxChange("convicted", e.target.checked)
+                }
+              />
+            </div>
           </div>
           <div className="inputInfoH">
-            {toggleOn2 && (
+            {formData.convicted.checked && (
               <input
                 className="inputReset4"
                 type="text"
                 placeholder="WHEN & WHY"
+                value={formData.convicted.details}
+                onChange={(e) =>
+                  handleDetailsChange("convicted", e.target.value)
+                }
               />
             )}
           </div>
+          {/* half */}
           <div className="leftSideInfo d-flex">
             <p className="infoP">Have You Ever Filed For Bankruptcy?</p>
             <div className="form-check form-switch">
@@ -63,22 +120,24 @@ const OtherInfo = () => {
                 className="form-check-input"
                 type="checkbox"
                 role="switch"
-                id="flexSwitchCheckDefault"
+                id="flexSwitchCheckDefault3"
+                checked={formData.bankruptcy.checked}
+                onChange={(e) =>
+                  handleCheckboxChange("bankruptcy", e.target.checked)
+                }
               />
-              <label
-                className="form-check-label"
-                htmlFor="flexSwitchCheckDefault"
-              >
-                Default switch checkbox input
-              </label>
             </div>
           </div>
           <div className="inputInfoH">
-            {toggleOn3 && (
+            {formData.bankruptcy.checked && (
               <input
                 className="inputReset4"
                 type="text"
                 placeholder="WHEN & WHY"
+                value={formData.bankruptcy.details}
+                onChange={(e) =>
+                  handleDetailsChange("bankruptcy", e.target.value)
+                }
               />
             )}
           </div>
@@ -91,14 +150,12 @@ const OtherInfo = () => {
                 className="form-check-input"
                 type="checkbox"
                 role="switch"
-                id="flexSwitchCheckDefault"
+                id="flexSwitchCheckDefault4"
+                checked={formData.smoke.checked}
+                onChange={(e) =>
+                  handleCheckboxChange("smoke", e.target.checked)
+                }
               />
-              <label
-                className="form-check-label"
-                htmlFor="flexSwitchCheckDefault"
-              >
-                Default switch checkbox input
-              </label>
             </div>
           </div>
           <div className="leftSideInfo rs d-flex">
@@ -108,40 +165,43 @@ const OtherInfo = () => {
                 className="form-check-input"
                 type="checkbox"
                 role="switch"
-                id="flexSwitchCheckDefault"
+                id="flexSwitchCheckDefault5"
+                checked={formData.pets.checked}
+                onChange={(e) => handleCheckboxChange("pets", e.target.checked)}
               />
-              <label
-                className="form-check-label"
-                htmlFor="flexSwitchCheckDefault"
-              >
-                Default switch checkbox input
-              </label>
             </div>
           </div>
           <div className="inputInfoH">
-            {toggleOn5 && (
+            {formData.pets.checked && (
               <span className="pets">
                 PLEASE LIST EACH TYPE, BREED & APPROX. WEIGHT
               </span>
             )}
-            {toggleOn5 && (
+            {formData.pets.checked && (
               <input
-                className="inputReset4 pet"
+                className="inputReset4 pet "
                 type="text"
                 placeholder="Cat, Persian, 3.6 Kg"
+                value={formData.pets.petDetails}
+                onChange={(e) => handlePetDetailsChange(e.target.value)}
               />
             )}
           </div>
-          <div className="inputR">
+          <div className="inputR mt-5">
             <input
               className="inputReset4 rightF"
               type="text"
               placeholder="HOW DID YOU LEARN ABOUT US?"
+              value={formData.learnAboutUs}
+              onChange={(e) => handleLearnAboutUsChange(e.target.value)}
             />
           </div>
         </div>
       </div>
-      <button className="bgButton d-flex align-items-center justify-content-center">
+      <button
+        className="bgButton d-flex align-items-center justify-content-center"
+        onClick={handleSubmit}
+      >
         <span className="submitBtn">Submit</span>
       </button>
     </div>
