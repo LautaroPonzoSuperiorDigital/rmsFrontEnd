e98f1d22da87f8f0e7c38dd46afb94a6b1f5e43a
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-12-06 19:00:34 -0300

    Update loader.js

--- src/pages/tenants/documents/loader.js
+++ src/pages/tenants/documents/loader.js
@@ -1,49 +1,67 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { DateTime } from "luxon"
+import { DateTime } from "luxon";
 
-import { api } from "../../../services/api"
-import { formatDate } from "../../../services/date"
-import { redirect } from "react-router-dom"
+import { api } from "../../../services/api";
+import { formatDate } from "../../../services/date";
+import { redirect } from "react-router-dom";
 
 const createDocumentsLoader = (signOut) => async () => {
-  let tenantId
-  let userEmail
+  let tenantId;
+  let userEmail;
 
   try {
-    const { data: loggedUser } = await api.get('/auth/local/session')
+    const { data: loggedUser } = await api.get("/auth/local/session");
 
-    tenantId = loggedUser.tenantId
-    userEmail = loggedUser.email
+    tenantId = loggedUser.tenantId;
+    userEmail = loggedUser.email;
   } catch (err) {
-    signOut()
-    return redirect('/')
+    signOut();
+    return redirect("/");
   }
 
-  const { data: sentDocuments } = await api.get(`/tenant/${tenantId}/document/sent`)
+  const { data: sentDocuments } = await api.get(
+    `/tenant/${tenantId}/document/sent`
+  );
 
   const tenantDocuments = await Promise.all(
-    sentDocuments
-      .map(sentDocument =>
-        api.get(`/tenant/${tenantId}/document/${sentDocument.Document.docId}/detail`)
-          .then(({ data: documentDetail }) => {
-            console.log(documentDetail, userEmail)
-            const applicantRecipient = documentDetail.recipients.find(recipient => recipient.email === userEmail)
-            const signUrl = applicantRecipient?.sharedLink
-            const isSigned = applicantRecipient?.hasCompleted
-            
-            return {
-              ...sentDocument,
-              status: documentDetail.status,
-              signUrl,
-              isSigned,
-              date: formatDate({ date: sentDocument.createdAt, formatOptions: DateTime.DATE_MED })
-            }
-          }))
-  )
+    sentDocuments.map(async (sentDocument) => {
+      try {
+        const { data: documentDetail } = await api.get(
+          `/tenant/${tenantId}/document/${sentDocument.Document.docId}/detail`
+        );
 
-  console.log(tenantDocuments)
+        console.log(documentDetail, userEmail);
 
-  return tenantDocuments
-}
+        const applicantRecipient = documentDetail.recipients.find(
+          (recipient) => recipient.email === userEmail
+        );
+        const signUrl = applicantRecipient?.sharedLink;
+        const isSigned = applicantRecipient?.hasCompleted;
 
-export default createDocumentsLoader
+        return {
+          ...sentDocument,
+          status: documentDetail.status,
+          signUrl,
+          isSigned,
+          date: formatDate({
+            date: sentDocument.createdAt,
+            formatOptions: DateTime.DATE_MED,
+          }),
+        };
+      } catch (error) {
+        console.error(`Error fetching document details: ${error.message}`);
+        return {
+          ...sentDocument,
+          status: "Error",
+          error: error.message,
+        };
+      }
+    })
+  );
+
+  console.log(tenantDocuments);
+
+  return tenantDocuments;
+};
+
+export default createDocumentsLoader;
